7c6497369d3eea3f55ebee5497a7bcd500b7417b
Olga Pysarenko <ola.shevchuk@gmail.com>
2023-11-07 11:27:25 +0200

    update route in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/sakurassweets-frontend">
+    <BrowserRouter>
      <App />
     </BrowserRouter>
   </React.StrictMode>,
