788c95233c6878d753d4af128862acebea35df20
Olga Pysarenko <ola.shevchuk@gmail.com>
2023-11-07 02:13:58 +0200

    added navigation

--- src/UserRoutes.tsx
+++ src/UserRoutes.tsx
@@ -1,39 +1,32 @@
 import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-const MainPage = lazy(() => import("./pages/MainPage/MainPage"));
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
+const Category = lazy(() => import("./pages/Сategory/Сategory"));
+const Favorites = lazy(() => import("./pages/Favorites/Favorites"));
+const Blog = lazy(() => import("./pages/Blog/Blog"));
+const AboutUs = lazy(() => import("./pages/AboutUs/AboutUs"));
 
 
 const UserRoutes = () => {
   return (
-    <Suspense fallback={<div>...here will be Loader Component</div>}>
+    <Suspense fallback={<p>...loading</p>}>
       <Routes>
-         <Route
-          path="/welcome"
+      <Route path="/" element={<HomePage />} />
+        <Route path="/about-us" element={<AboutUs/>} />
+        <Route
+          path="/category"
           element={
             // <PublicRoute>
-              <MainPage/>
+              <Category/>
             // </PublicRoute>
           }
         />
-        {/* <Route
-          path="/signup"
-          element={
-            <PublicRoute>
-              <SignupPage />
-            </PublicRoute>
-          }
-        /> */}
-        
+        <Route path="/favorites" element={<Favorites/>} />
+        <Route path="/blog" element={<Blog/>} />     
        
-          <Route
-            path="*"
-            element={
-              // <PublicRoute>
-                <NotFoundPage />
-              // </PublicRoute>
-            }
-          />
+        <Route path="*" element={<NotFoundPage />} />
          
       </Routes>
     </Suspense>
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom";
+import styles from './Navigation.module.css';
+
+const Navigation = () => {
+    return (
+      <ul className={styles.menu}> 
+        <NavLink to="/">
+          Home
+        </NavLink>  
+        <NavLink to="/about-us">
+          About Us
+        </NavLink>
+        <NavLink to="/blog">
+          Blog
+        </NavLink>     
+        <NavLink to="/favorites">
+          Favorites
+        </NavLink>
+      </ul>
+    );
+  };
+  
+  export default Navigation;
