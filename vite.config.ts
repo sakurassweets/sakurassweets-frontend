c8b8362ae054dbe70782d478aad295203f370937
Yelyzaveta <94306589+ElizabethVasilenko13@users.noreply.github.com>
2023-11-07 12:45:16 +0300

    chore: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: "/sakurassweets-frontend",
+  base: "/",
   plugins: [react()],
   preview: {
     port: 8000,
