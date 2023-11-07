0f537bd696bdc20f64bf7db68e1b024719138979
parasolya <113554970+parasolya@users.noreply.github.com>
2023-11-07 02:16:24 +0200

    added navigation (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import './App.css'
+import './App.css';
+import Navigation from "./components/Navigation/Navigation";
+import UserRoutes from './UserRoutes';
 
 function App() {
   console.log('Hello world');
   fetch('https://api.sakurassweets.asion.tk/').then(res => res.json()).then(data => console.log(data.message))
   return (
-    <div>Hello! I`m Frontend!</div>
+    <div>
+      <Navigation/>
+      <UserRoutes />
+    </div>
     
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: "/",
+  base: "/sakurassweets-frontend",
   plugins: [react()],
   preview: {
     port: 8000,
