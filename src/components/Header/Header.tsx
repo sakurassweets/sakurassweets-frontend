9f844f10261d527a311a8b6f618203f97b63dfae
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2023-11-20 22:17:40 +0200

    Remove subheader

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,8 @@
 import { MainHeader } from './MainHeader/MainHeader';
-import { SubHeader } from './SubHeader/SubHeader';
 
 export const Header: React.FC = () => {
   return (
     <header>
-      <SubHeader />
       <MainHeader />
     </header>
   );
