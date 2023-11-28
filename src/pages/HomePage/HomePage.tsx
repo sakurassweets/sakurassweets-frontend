2db8bddfb2972b0162c74623e7a8dd878f30e322
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2023-11-29 00:57:44 +0200

    EAM-92-add-product-card

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,11 @@
+import { ProductCard } from '../../components/ProductCard/ProductCard';
+
 const MainPage = () => {
   return (
     <main>
       <div className="container">
         <h1>Main Page</h1>
+        <ProductCard />
       </div>
     </main>
   );
