194e9a8824ea3b404c8057ae267c1cf14447521b
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2023-12-03 23:29:13 +0200

    Fix images

--- src/components/HomePaige/AdvantagesList/AdvantageItem/AdvantageItem.tsx
+++ src/components/HomePaige/AdvantagesList/AdvantageItem/AdvantageItem.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
 import classes from './AdvantageItem.module.scss';
-import config from './advantageItemConfig';
 
 interface Props {
   advantage: {
@@ -14,10 +13,7 @@ interface Props {
 export const AdvantageItem: React.FC<Props> = ({ advantage: { icon, header, text } }) => {
   return (
     <div className={classNames(classes.advantageItem)}>
-      <div
-        className={classNames(classes.advantageIcon)}
-        style={{ backgroundImage: `url(${config.imagePath + icon})` }}
-      ></div>
+      <div className={classNames(classes.advantageIcon)} style={{ backgroundImage: `url(${icon})` }}></div>
       <div className={classNames(classes.advantageText)}>
         <p className={classNames(classes.advantageHeader)}>{header}</p>
         <p>{text}</p>
--- src/components/HomePaige/AdvantagesList/AdvantagesList.tsx
+++ src/components/HomePaige/AdvantagesList/AdvantagesList.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import classNames from 'classnames';
 import classes from './AdvantagesList.module.scss';
+import trackUrl from '../../../assets/icons/track.svg';
+import phoneUrl from '../../../assets/icons/phone.svg';
+import bagUrl from '../../../assets/icons/bag.svg';
+import cashUrl from '../../../assets/icons/cash.svg';
 
 import { AdvantageItem } from './AdvantageItem/AdvantageItem';
 
 const advantages = [
   {
-    icon: 'icons/track.svg',
+    icon: trackUrl,
     header: 'Безкоштовна доставка',
     text: 'Безкоштовна доставка по всій Україні',
   },
   {
-    icon: 'icons/phone.svg',
+    icon: phoneUrl,
     header: 'Підтримка 24/7',
     text: 'Передзвонимо вам протягом хвилини',
   },
   {
-    icon: 'icons/bag.svg',
+    icon: bagUrl,
     header: 'Гарантія повернення ',
     text: 'Повернення товару протягом 30 днів',
   },
   {
-    icon: 'icons/cash.svg',
+    icon: cashUrl,
     header: '100% безпечна оплата',
     text: 'Ми гарантуємо безпеку ваших грошей',
   },
