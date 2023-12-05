756b00675a99e1742deef31a9dbca377db2f78bc
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2023-12-05 17:58:42 +0200

    EAM 90 Add Slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,26 +1,30 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import classNames from 'classnames';
 import classes from './Slider.module.scss';
 
 import { SliderBtn } from './SliderBtn/SliderBtn';
+import { Product } from '../../types/interfaces/Product';
+import { ProductCard } from '../ProductCard/ProductCard';
 
 const DIRECTION = {
   LEFT: 'left',
   RIGHT: 'right',
 };
 
-const name = 'Акції';
+interface Props {
+  name: string;
+  items: Product[];
+  marginBottom: number;
+}
 
-const ex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-
-export const Slider = () => {
+export const Slider: React.FC<Props> = ({ name, items, marginBottom }) => {
   const [pos, setPos] = useState(0);
 
   function handleClick(direction: string) {
     const pol = direction === DIRECTION.LEFT ? 1 : -1;
     if (pol === 1 && pos === 0) {
-      setPos(-2934);
+      setPos(-1 * (items.length - 3) * 326);
     }
     if (pol === -1 && pos === -2608) {
       setPos(0);
@@ -30,7 +34,7 @@ export const Slider = () => {
   }
 
   return (
-    <div className={classNames(classes.slider_container)}>
+    <div className={classNames(classes.slider_container)} style={{ marginBottom: `${marginBottom}px` }}>
       <div className={classNames(classes.slider_header)}>
         <p className={classNames(classes.slider_name)}>{name}</p>
         <NavLink to="/catalog" className={classNames(classes.slider_link)}>
@@ -38,17 +42,23 @@ export const Slider = () => {
         </NavLink>
       </div>
       <div className={classNames(classes.slider_content)}>
-        <SliderBtn direction={DIRECTION.LEFT} onClick={(direction: string) => handleClick(direction)} />
+        <SliderBtn
+          direction={DIRECTION.LEFT}
+          onClick={(direction: string) => handleClick(direction)}
+          display={items.length > 4}
+        />
         <div className={classNames(classes.slider_window)}>
           <div className={classNames(classes.slider_cards)} style={{ transform: `translate(${pos}px)` }}>
-            {ex.map((card) => (
-              <div className={classNames(classes.item)} key={Date.now() + Math.random() + card}>
-                Item{card}
-              </div>
+            {items.map((card) => (
+              <ProductCard product={card} />
             ))}
           </div>
         </div>
-        <SliderBtn direction={DIRECTION.RIGHT} onClick={(direction: string) => handleClick(direction)} />
+        <SliderBtn
+          direction={DIRECTION.RIGHT}
+          onClick={(direction: string) => handleClick(direction)}
+          display={items.length > 4}
+        />
       </div>
     </div>
   );
