ddcbda4fe874bb2efeddd07bdafc0055ca710b40
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2024-01-13 16:26:09 +0100

    EAM-125 add show/hide icon for input

--- src/components/Input/FormikInput.tsx
+++ src/components/Input/FormikInput.tsx
@@ -1,6 +1,9 @@
 import classNames from 'classnames';
 import { useField } from 'formik';
 import classes from './Input.module.scss';
+import iconShow from '../../assets/icons/eye-password-show.svg';
+import iconClose from '../../assets/icons/eye-password-close.svg';
+import { useState } from 'react';
 
 interface InputFormikProps {
   label: string;
@@ -13,6 +16,8 @@ interface InputFormikProps {
 }
 
 export const InputFormik: React.FC<InputFormikProps> = ({ label, ...props }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const [field, meta] = useField(props);
 
   const containerClasses = classNames(classes.formControl);
@@ -20,12 +25,19 @@ export const InputFormik: React.FC<InputFormikProps> = ({ label, ...props }) => 
   const inputClasses = classNames(classes.inputContainer, props.className, {
     [classes.inputError]: meta.touched && meta.error,
   });
+  const handleToggleEye = () => {
+    setIsOpen((prev) => !prev);
+  };
 
   return (
     <div className={containerClasses}>
       <label className={classes.labelsWrapper}>{label}</label>
       <div className={inputClasses}>
-        <input {...field} {...props} />
+        <input {...field} {...props} type={!isOpen && props.type === 'password' ? props.type : 'text'} />
+
+        {props.type === 'password' && (
+          <img src={isOpen ? iconShow : iconClose} className={classes.svg} onClick={handleToggleEye} />
+        )}
       </div>
       {meta.touched && meta.error && <div className={classes.errorMessage}>{meta.error}</div>}
     </div>
