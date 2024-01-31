8f27f776e2c4f396aa2a8dcaf2067b7a1ebd5825
Stanislav Ivanov <103877318+stanislaviw@users.noreply.github.com>
2024-01-31 16:57:17 +0200

    SAK-57 Added new rules to README (#66)
    Added rules for git worflow
    Added rules for compontents declaration

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,11 +2,11 @@ import { Footer } from './Footer/Footer';
 import { Header } from './Header/Header';
 import classes from './Layout.module.scss';
 
-interface Props {
+interface LayoutProps {
   children: React.ReactNode;
 }
 
-export const Layout: React.FC<Props> = ({ children }): JSX.Element => {
+export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className={classes.layout}>
       <Header />
