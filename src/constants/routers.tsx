5375f81b980ccc2a27709d2322aad1e946932a75
AndriySuprunenko <43989070+AndriySuprunenko@users.noreply.github.com>
2024-02-26 17:00:04 +0200

    refactor

---
+++ src/constants/routers.tsx
@@ -0,0 +1,11 @@
+export const ROUTERS = {
+  HOME: '/',
+  CATALOG: '/catalog',
+  ABOUT_US: '/about-us',
+  FAVORITES: '/favorites',
+  CART: '/cart',
+  PRODUCT: '/product/:id',
+  ACCOUNT: '/account',
+  DOCS: '/docs/:name',
+  NOT_FOUND: '*',
+};
