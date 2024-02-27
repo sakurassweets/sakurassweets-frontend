ed7f06c1a7e095f047b5ac055f825ec45c2a7352
AndriySuprunenko <43989070+AndriySuprunenko@users.noreply.github.com>
2024-02-27 15:29:19 +0200

    bug fix

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import classNames from 'classnames';
+
 import classes from './Hero.module.scss';
+import { ROUTERS } from '../../../constants/routers';
 
 export const Hero: React.FC = () => {
   return (
@@ -9,7 +11,7 @@ export const Hero: React.FC = () => {
       <div className={classNames(classes.hero_content, 'container')}>
         <h1 className={classNames(classes.hero_header)}>Магазин японських солодощів</h1>
         <p className={classNames(classes.hero_text)}>Унікальний вибір традиційних та сучасних смаколиків</p>
-        <NavLink to="/catalog" className={classNames(classes.hero_btn)}>
+        <NavLink to={ROUTERS.CATALOG} className={classNames(classes.hero_btn)}>
           Біжу купляти
         </NavLink>
       </div>
