6bacc1e65a8c225bbfb8da8e9a91d44ad2a01cce
AndriySuprunenko <43989070+AndriySuprunenko@users.noreply.github.com>
2024-03-11 11:46:14 +0200

    Feature

--- src/components/Catalog/filter/Filter.tsx
+++ src/components/Catalog/filter/Filter.tsx
@@ -5,6 +5,7 @@ import { Price } from './contents/price/Price';
 import { Category } from './contents/category/Category';
 
 import styled from './filter.module.scss';
+import { Raiting } from './contents/raiting/Raiting';
 
 export const Filter: React.FC = () => {
   return (
@@ -12,7 +13,7 @@ export const Filter: React.FC = () => {
       <h1 className={styled.title}>Фільтри</h1>
       <Accordion title="Ціна" content={<Price minValue={0} maxValue={20000} />} id={1} />
       <Accordion title="Категорія" content={<Category />} id={2} />
-      <Accordion title="Рейтинг" content="Content" id={3} />
+      <Accordion title="Рейтинг" content={<Raiting />} id={3} />
     </>
   );
 };
