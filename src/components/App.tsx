5581c8e90a43cf4ae3aa583008dd4a82b23ebf9a
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2024-03-17 12:42:07 +0100

    Refactor imports in App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,9 @@
 import UserRoutes from './UserRoutes';
-import { useAppDispatch } from '../redux/hook';
 import { useEffect } from 'react';
+import { useAppDispatch } from '../redux/hook';
+import { useLocation } from 'react-router-dom';
 import { refreshThunk } from '../redux/auth/operations';
 import { Layout } from './Layout/Layout';
-import { useLocation } from 'react-router-dom';
 
 function App() {
   const dispatch = useAppDispatch();
