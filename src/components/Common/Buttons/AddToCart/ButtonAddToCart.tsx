e5cc61d75848da78dca985a6298e000f8b58bdfb
Krasnoshlik <dimakrasnoshluk@gmail.com>
2024-03-20 17:15:20 +0200

    first fun

--- src/components/Common/Buttons/AddToCart/ButtonAddToCart.tsx
+++ src/components/Common/Buttons/AddToCart/ButtonAddToCart.tsx
@@ -1,16 +1,36 @@
 import { FiShoppingCart } from 'react-icons/fi';
+import { ItemAddedinCart } from './ItemAddedinCart/ItemAddedinCart';
+import { useEffect, useState } from 'react';
 
 import classes from './buttonAddToCart.module.scss';
 
 export const ButtonAddToCart = () => {
   const onAddToCart = () => {
     console.log('Added to cart');
+    setShowPopUp(true);
   };
 
+  const [showPopUp, setShowPopUp] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowPopUp(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [showPopUp]);
+
+  let popup = null;
+  if (showPopUp) {
+    popup = <ItemAddedinCart />;
+  }
+
   return (
-    <button className={classes.button} onClick={onAddToCart}>
-      <FiShoppingCart className={classes.button__icon} />
-      Додати до кошика
-    </button>
+    <>
+      {popup}
+      <button className={classes.button} onClick={onAddToCart}>
+        <FiShoppingCart className={classes.button__icon} />
+        Додати до кошика
+      </button>
+    </>
   );
 };
