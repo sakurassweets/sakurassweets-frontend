7b210cf1fd78ef0bd645a4b16d2e4540edbf399e
Dmytro Krasnoshlyk <144529402+Krasnoshlik@users.noreply.github.com>
2024-03-21 18:59:42 +0200

    SAK-77 add modal el for add-to-cart button  (#84)

--- src/components/Common/Buttons/AddToCart/ButtonAddToCart.tsx
+++ src/components/Common/Buttons/AddToCart/ButtonAddToCart.tsx
@@ -1,16 +1,34 @@
 import { FiShoppingCart } from 'react-icons/fi';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 import classes from './buttonAddToCart.module.scss';
 
 export const ButtonAddToCart = () => {
   const onAddToCart = () => {
     console.log('Added to cart');
+    setShowPopUp(true);
   };
 
+  const [showPopUp, setShowPopUp] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowPopUp(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [showPopUp]);
+
+  if (showPopUp) {
+    toast.success(`Товар успішно доданий`);
+  }
+
   return (
-    <button className={classes.button} onClick={onAddToCart}>
-      <FiShoppingCart className={classes.button__icon} />
-      Додати до кошика
-    </button>
+    <>
+      <button className={classes.button} onClick={onAddToCart}>
+        <FiShoppingCart className={classes.button__icon} />
+        Додати до кошика
+      </button>
+    </>
   );
 };
