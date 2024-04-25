82c304330b6f372961f9c45b72b70b7f21b85336
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2024-04-25 12:50:53 +0200

    SAK-87 created Order Success Modal

---
+++ src/components/Cart/components/index.ts
@@ -0,0 +1,5 @@
+import { CartTable } from './cartTable/CartTable';
+import { FreeDelivery } from './freedelivery/FreeDelivery';
+import { CartStub } from './stub/CartStub';
+
+export { CartTable, FreeDelivery, CartStub };
