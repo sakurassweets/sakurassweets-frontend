d14e2d53fac96d1d0cdf4a061e63e61807a8c17e
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2024-05-19 11:24:18 +0200

    fix: updated layout styles, added manufacturer to Product interface

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,5 @@
 import { Footer } from './Footer/Footer';
 import { Header } from './Header/Header';
-import classes from './layout.module.scss';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -10,7 +9,7 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
       <Header />
-      <main className={classes.content}>{children}</main>
+      <main>{children}</main>
       <Footer />
     </>
   );
